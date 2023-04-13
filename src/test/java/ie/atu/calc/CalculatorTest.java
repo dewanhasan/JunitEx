a839fb79a82f7e2482f17a96899e25146319b4b2
dewanhasan99 <g00410156@atu.ie>
2023-04-13 15:12:35 +0100

    Second Commit

--- src/test/java/ie/atu/calc/CalculatorTest.java
+++ src/test/java/ie/atu/calc/CalculatorTest.java
@@ -1,4 +1,17 @@
 package ie.atu.calc;
 
+import ie.atu.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator myCalc = new Calculator();
+    @Test
+    void testAdd()
+    {
+        myCalc=new Calculator();
+        assertEquals(40, myCalc.add(20,20));
+
+    }
 }
